5847b14e7e79a792dca5c659937d7aaf5af3d53a
Данила <danilalogunov13@gmail.com>
2021-06-28 22:46:46 +0300

    v4

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -111,7 +111,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    "PAGE_SIZE": 100
+    'PAGE_SIZE': 100
 }
 
 AUTH_USER_MODEL = 'authentication.User'
